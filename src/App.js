b1fcd747da674170e087f0b66bfc89813262babf
Jarvis <jarvismiranda@gmail.com>
2022-03-22 19:33:32 -0500

    subiendo redux

--- src/App.js
+++ src/App.js
@@ -1,25 +1,27 @@
 import "./App.css";
 import Header from "./components/Header";
-import {
-  BrowserRouter,
-  Routes,
-  Route,
-} from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Productos from "./components/Productos";
 import NuevoProducto from "./components/NuevoProducto";
 import EditarProducto from "./components/EditarProducto";
 
+//Redux
+import { Provider } from "react-redux";
+import store from "./store";
+
 function App() {
   return (
     <BrowserRouter>
-      <Header />
-      <div className="container mt-5">
-        <Routes>
-          <Route path="/" element={<Productos />} />
-          <Route path="/productos/nuevo" element={<NuevoProducto />} />
-          <Route path="/productos/editar/:id" element={<EditarProducto />} />
-        </Routes>
-      </div>
+      <Provider store={store}>
+        <Header />
+        <div className="container mt-5">
+          <Routes>
+            <Route path="/" element={<Productos />} />
+            <Route path="/productos/nuevo" element={<NuevoProducto />} />
+            <Route path="/productos/editar/:id" element={<EditarProducto />} />
+          </Routes>
+        </div>
+      </Provider>
     </BrowserRouter>
   );
 }
--- src/components/NuevoProducto.js
+++ src/components/NuevoProducto.js
@@ -1,7 +1,71 @@
-import React from "react";
+import { useState } from "react";
+import { crearNuevoProducto } from "../actions/productoActions";
+import { useDispatch, useSelector } from "react-redux";
 
 const NuevoProducto = () => {
-  return <h1>NuevoProducto</h1>;
+  const [nombre, setNombre] = useState("");
+  const [precio, setPrecio] = useState("");
+  //utilizar use dispatch y te crea una funciona de
+  const dispatch = useDispatch();
+  const agregarProducto = (producto) => dispatch(crearNuevoProducto(producto));
+  const submitNuevoProducto = (e) => {
+    e.preventDefault();
+    console.log("Prueba");
+    //Validar Formulario
+    if (nombre.trim() === "" || precio.trim <= "") {
+      return;
+    }
+
+    //Si no hay errores
+
+    //crear el nuevo producto
+    agregarProducto({ nombre, precio });
+  };
+  return (
+    <div className="row justify-content-center ">
+      <div className="col-md-8">
+        <div className="card">
+          <div className="card-body">
+            <h2 className="text-center mb-4 font-weight-bold">
+              Agregar Nuevo Producto
+            </h2>
+            <form onSubmit={submitNuevoProducto}>
+              <div className="form-group">
+                <label htmlFor="nombreProducto">Nombre Producto</label>
+                <input
+                  type="text"
+                  className="form-control"
+                  placeholder="Nombre Producto"
+                  id="nombreProducto"
+                  name="nombreProducto"
+                  value={nombre}
+                  onChange={(e) => setNombre(e.target.value)}
+                />
+              </div>
+              <div className="form-group">
+                <label htmlFor="precioProducto">Precio Producto</label>
+                <input
+                  type="number"
+                  className="form-control"
+                  placeholder="Precio Producto"
+                  id="precioProducto"
+                  name="precioProducto"
+                  value={precio}
+                  onChange={(e) => setPrecio(Number(e.target.value))}
+                />
+              </div>
+              <button
+                type="submit"
+                className="btn btn-primary font-weight-bold text-uppercase d-block w-100"
+              >
+                Agregar
+              </button>
+            </form>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default NuevoProducto;
